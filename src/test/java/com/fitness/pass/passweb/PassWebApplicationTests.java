694a9b9e0585fcc097c4fb6bfe8478c94da5a2db
Kyle Kim <dkim577@wisc.edu>
2024-06-29 18:54:38 +0900

    First Commit

---
+++ src/test/java/com/fitness/pass/passweb/PassWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.pass.passweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
